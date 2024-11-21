994e3bfeb68b786dedffb38fd5fc5b3ffb2d44bb
platoncyber <platon.masterskaya@gmail.com>
2024-11-21 10:53:01 +0300

    update game

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 let app = document.querySelector('.app');
 
+let name_people = document.querySelector('#name');
+
 const hamster = document.querySelector('.display_tap');
 
 const currentWallet = document.querySelector('#wallet_coin');
@@ -10,6 +12,19 @@ const lvlProgress = document.querySelector('.level_progress');
 
 const level = document.querySelector('#level');
 
+const nav_mine = document.querySelector('.display_footer-mine');
+
+const nav_game = document.querySelector('.display_footer-exchange');
+
+const display_content = document.querySelector('.display_content');
+
+const mine_content = document.querySelector('.mine_content');
+
+const buyButtons = document.querySelectorAll('.buy_card');
+
+console.log(buyButtons);
+
+let buy_display = document.querySelector('.buy_display');
 
 
 const data = {
@@ -27,7 +42,9 @@ function handleGreeting(){
     let img = document.createElement('img');
     img.src = '../img/hamdter_poster.png';
     div.classList.add('greating');
-    
+    let value = prompt('введите ваше имя')
+
+    name_people.innerHTML = value;
 
     div.appendChild(img);
 
@@ -39,6 +56,31 @@ function handleGreeting(){
      
     },6000)
 }
+
+
+buyButtons.forEach((value, index, array)=>{
+    value.addEventListener('click',function(){
+        let price = value.dataset.price;
+        
+        buy_display.style.display = 'flex';
+        const buy_btn = document.querySelector('.buy_btn');
+        buy_btn.onclick = function(){
+            if(price < data.coin){
+                data.coin = data.coin - price;
+                data.profit = data.profit + 200;
+            }
+
+            buy_display.style.display = 'none'
+        }
+      
+
+    })
+})
+
+
+
+
+
 
 
 function handleTap(e){
@@ -125,6 +167,15 @@ function energyRecovery(){
 
 
 
+function changeToMenu(){
+    mine_content.style.display = 'flex'
+        display_content.style.display = 'none'
+}
+
+function changeToGame(){
+    mine_content.style.display = 'none'
+        display_content.style.display = 'flex'
+}
 
 
 
@@ -151,6 +202,6 @@ energyRecovery();
 
 
 
-
-
+nav_mine.addEventListener('click',changeToMenu);
+nav_game.addEventListener('click',changeToGame);
 hamster.addEventListener('click',handleTap);
